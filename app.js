e73a061fcff4c01541744ab617ffd46c09639b33
Marie Kry <mariekry@gmail.com>
2025-11-27 11:38:53 +0100

    Update app.js
    Tog bort numreringen på ljudfilerna samt ändelsen B på varje.

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const coverImage = "https://via.placeholder.com/150";
 const tracks = [
-{ title: "2. Koncentration B", url: "https://hogrefe.se/userfiles/files/2__Koncentration_B.mp3" },
-{ title: "4. Andningstrappan B", url: "https://hogrefe.se/userfiles/files/4__Andningstrappan_B.mp3" },
-{ title: "6. Bodyscan B", url: "https://hogrefe.se/userfiles/files/6__Bodyscan_B.mp3" },
-{ title: "8. Surfa på känslan B", url: "https://hogrefe.se/userfiles/files/8__Surfa_pa%CC%8A_ka%CC%88nslan_B.mp3" },
-{ title: "10. Att komma hit B", url: "https://hogrefe.se/userfiles/files/10__Att_komma_hit_B.mp3" },
-{ title: "12. Småleende B", url: "https://hogrefe.se/userfiles/files/12__Sma%CC%8Aleende_B.mp3" },
-{ title: "14. Tankar som moln B", url: "https://hogrefe.se/userfiles/files/14__Tankar_som_moln_B.mp3" },
-{ title: "16. Bioduken B", url: "https://hogrefe.se/userfiles/files/16__Bioduken_B.mp3" },
-{ title: "18. Det inre rummet B", url: "https://hogrefe.se/userfiles/files/18__Det_inre_rummet_B.mp3" },
-{ title: "20. Den långa listan B", url: "https://hogrefe.se/userfiles/files/20__Den_la%CC%8Anga_listan_B.mp3" },
-{ title: "22. Andning i benet och där det gör ont B", url: "https://hogrefe.se/userfiles/files/22__Andning_i_benet_och_da%CC%88r_det_go%CC%88r_ont_B.mp3" },
-{ title: "24. Fokus på relationsproblem B", url: "https://hogrefe.se/userfiles/files/24__Fokus_pa%CC%8A_relationsproblem_B.mp3" },
+{ title: "Koncentration", url: "https://hogrefe.se/userfiles/files/2__Koncentration_B.mp3" },
+{ title: "Andningstrappan", url: "https://hogrefe.se/userfiles/files/4__Andningstrappan_B.mp3" },
+{ title: Bodyscan", url: "https://hogrefe.se/userfiles/files/6__Bodyscan_B.mp3" },
+{ title: "Surfa på känslan", url: "https://hogrefe.se/userfiles/files/8__Surfa_pa%CC%8A_ka%CC%88nslan_B.mp3" },
+{ title: "Att komma hit", url: "https://hogrefe.se/userfiles/files/10__Att_komma_hit_B.mp3" },
+{ title: "Småleende", url: "https://hogrefe.se/userfiles/files/12__Sma%CC%8Aleende_B.mp3" },
+{ title: "Tankar som moln", url: "https://hogrefe.se/userfiles/files/14__Tankar_som_moln_B.mp3" },
+{ title: "Bioduken", url: "https://hogrefe.se/userfiles/files/16__Bioduken_B.mp3" },
+{ title: "Det inre rummet", url: "https://hogrefe.se/userfiles/files/18__Det_inre_rummet_B.mp3" },
+{ title: "Den långa listan", url: "https://hogrefe.se/userfiles/files/20__Den_la%CC%8Anga_listan_B.mp3" },
+{ title: "Andning i benet och där det gör ont", url: "https://hogrefe.se/userfiles/files/22__Andning_i_benet_och_da%CC%88r_det_go%CC%88r_ont_B.mp3" },
+{ title: "Fokus på relationsproblem", url: "https://hogrefe.se/userfiles/files/24__Fokus_pa%CC%8A_relationsproblem_B.mp3" },
 ];
 // ----------------------------------------------------------
 
